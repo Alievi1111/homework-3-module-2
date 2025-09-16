e7d61aebc81599e5ea3cb09af35c2e563c12325e
Alievi1111 <alieviali1111@gmail.com>
2025-09-16 14:16:58 +0400

    feature/jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="headerContainer">
+      <h2>Startup 3</h2>
+      <nav>
+        <a href="#">Tour</a>
+        <a href="#">Prices</a>
+        <a href="#">About</a>
+        <a href="#">F.A.Q.</a>
+        <a href="#">Support</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+function Main() {
+  return (
+    <main className="firstContainer">
+      <section className="topSection">
+        <h1>Build Your Own Site like a Lego Constructor</h1>
+        <p>
+          We have created a new product that will help designers, developers and
+          companies create websites for their startups quickly and easily.
+        </p>
+        <div className="buttons">
+          <button className="loginBtn">Log In</button>
+          <button className="signupBtn">Sign Up</button>
+        </div>
+      </section>
+
+      <section className="lastSection">
+        <div className="container">
+          <p>A high-quality solution for a beautiful startup website.</p>
+        </div>
+        <div className="container">
+          <p>
+            Startups can save money on design and code and use those savings to
+            develop the business.
+          </p>
+        </div>
+        <div className="container">
+          <p>
+            Startup Framework includes great form options for your startup
+            projects.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
